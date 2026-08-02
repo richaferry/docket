a3fe75f2e96eea8497fc300cea8ea923dd8a1d43
Richa Ferry Setyawan <49858604+richaferry@users.noreply.github.com>
2026-08-02 10:37:55 +0700

    update eslint (#2)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,14 @@ const eslintConfig = defineConfig([
     "next-env.d.ts",
     "coverage/**",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
